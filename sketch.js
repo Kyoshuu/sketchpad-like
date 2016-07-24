6173972b0cd293ee8606aede914258d5fc7feaed
Bartosz Zagozda <bzagozda@hotmail.com>
2016-07-24 17:16:14 +0200

    added drawing possibility and changing nodes number

--- sketch.js
+++ sketch.js
@@ -1,11 +1,38 @@
 $(document).ready(function() {
-	var $content = $("#content");
+	generate(16);
 
-	for(var i = 1; i <= 16 * 16; i++) {
-		$content.append('<div class="node"></div>');
+	$('#reset').hover(
+	function() {
+		$(this).css("border-width", "2px");
+	},
+	function() {
+		$(this).css("border-width", "1px");
+	});
+
+	$('#reset').click(function() {
+		var size = prompt("Size of sketch: ");
+
+		$('#grid').empty();
 		
-		if(i%16 == 0) {
-			$content.append('<div style="clear: both;"></div>');
+		generate(size);
+	});
+});
+
+function generate(size) {
+	for(var i = 1; i <= size * size; i++) {
+		$('#grid').append('<div class="node"></div>');
+		
+		if(i%size == 0) {
+			$('#grid').append('<div style="clear: both;"></div>');
 		}
 	}
-});
+
+	$('.node').hover(function() {
+		$(this).css('background-color', 'black');
+	});
+
+	var nodeSize = (544 - size * 4) / size;
+
+	$('.node').css("width", nodeSize + "px");
+	$('.node').css("height", nodeSize + "px");
+};
